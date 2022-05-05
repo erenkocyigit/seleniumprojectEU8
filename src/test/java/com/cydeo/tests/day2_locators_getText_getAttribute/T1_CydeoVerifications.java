8d715d7b415694c4cb77de12d1853c958cb43f6f
erenkocyigit <m.erenkocyigit@gmail.com>
2022-05-05 16:34:29 +0200

    Selenium Day 2

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -1,8 +1,39 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WdmAgent;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T1_CydeoVerifications {
 
+    /*
+    IN THIS CLASS WE ARE SOLVING THE DAY2_TASK1
+     */
+
+    public static void main(String[] args) {
+
+        //TC #1: Cydeo practice tool verifications
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
 
+
+        //2. Go to https://practice.cydeo.com
+
+
+        //3. Verify URL contains
+        //Expected: cydeo
+
+
+        // 4. Verify title:
+        //Expected: Practice
+
+
+
+
+    }
 
 }
