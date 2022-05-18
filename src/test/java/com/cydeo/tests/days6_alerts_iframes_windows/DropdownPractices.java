3bf43b1b7c2e5d01a8c484e01a8611b2e88e4f54
erenkocyigit <m.erenkocyigit@gmail.com>
2022-05-18 16:10:59 +0200

    Selenium Day 6

---
+++ src/test/java/com/cydeo/tests/days6_alerts_iframes_windows/DropdownPractices.java
@@ -0,0 +1,90 @@
+package com.cydeo.tests.days6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com/dropdown");
+
+    }
+
+    @Test
+    public void dropdown_task5(){
+
+
+        //We located the dropdown and it is ready to use
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+
+        //Select Illinois
+        stateDropdown.selectByVisibleText("Illinois");
+
+        //Select Virginia
+        stateDropdown.selectByValue("VA");
+
+        //Select California
+        stateDropdown.selectByIndex(5);
+
+        //Verify final selected option is California.
+        String expectedOptionText = "California";
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(expectedOptionText,actualOptionText);
+
+
+
+    }
+
+    @Test
+    public void dropdown_task6(){
+
+        //Select "December 1, "1923" and verify it is selected.
+
+        Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
+        Select dayDropdown = new Select(driver.findElement(By.xpath("//select[@id='day']")));
+
+        //Select year using : visible text.
+        yearDropdown.selectByVisibleText("1923");
+        //Select month using : value attribute
+        monthDropdown.selectByValue("11");
+        //Select day using : index number
+        dayDropdown.selectByIndex(0);
+
+        //creating expected values
+        String expectedYear= "1923";
+        String expectedMonth = "December";
+        String expectedDay = "1";
+
+        //getting actual values from browser
+        String actualYear = yearDropdown.getFirstSelectedOption().getText();
+        String actualMonth = monthDropdown.getFirstSelectedOption().getText();
+        String actualDay = dayDropdown.getFirstSelectedOption().getText();
+
+        //create assertions
+        Assert.assertTrue(actualYear.equals(expectedYear));
+        Assert.assertEquals(actualMonth,expectedMonth);
+        Assert.assertEquals(actualDay,expectedDay);
+
+
+
+    }
+
+
+
+}
